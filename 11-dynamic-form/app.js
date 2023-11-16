824395799262bbfeca1e36047c13bafa8c483542
luna-spr <luna@spreatics.com>
2023-11-16 13:55:41 +0900

    비동기 form

---
+++ 11-dynamic-form/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('dynamic');
+});
+
+app.get('/ajax', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post('/ajax', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.get('/axios', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post('/axios', (req, res) => {
+  console.log(req.body);
+
+  // res.send(req.body);
+  res.send({ name: req.body.name, gender: req.body.gender, msg: '반가워!!' });
+});
+
+app.get('/fetch', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post('/fetch', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, function () {
+  console.log(`Port ${PORT} is opening!`);
+});
