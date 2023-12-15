eeb3b852c3ad803073f9f9f321c162b1f33ae692
luna-spreatics <luna@spreatics.com>
2023-12-15 16:28:19 +0900

    ppt 맞게 수정

--- 25-state/src/Counter.js
+++ 25-state/src/Counter.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 // 클래스형 컴포넌트
 class Counter extends Component {
-  // 기존 버전
+  // (생략)기존 버전
   // constructor(props) {
   //   // - 자바스크립트에서 `super`는 부모클래스 생성자의 참조
-  //   // - 이 함수가 호출되면 현재 Counter 클래스가 상속받고 있는 리액트의 
+  //   // - 이 함수가 호출되면 현재 Counter 클래스가 상속받고 있는 리액트의
   //   //   Component 클래스(부모클래스)가 지닌 생성자 함수를 호출해 준답니다.
   //   super(props);
 
