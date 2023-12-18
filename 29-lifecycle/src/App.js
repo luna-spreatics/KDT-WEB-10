e05203a939ee1f5e63a93607d353081dd3af4833
luna-spreatics <luna@spreatics.com>
2023-12-18 17:03:15 +0900

    add react lifecycle code

---
+++ 29-lifecycle/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import LifeCycleClass from './LifeCycleClass';
+import LifeCycleFunc from './LifeCycleFunc';
+import PostList from './PostList';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 클래스형 컴포넌트 Life Cycle */}
+      <LifeCycleFunc />
+      <hr />
+
+      {/* 함수형 컴포넌트 Life Cycle */}
+      <LifeCycleClass />
+      <hr />
+
+      {/* 코딩온 실습 */}
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
