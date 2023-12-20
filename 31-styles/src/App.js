21f88b620ec2ce422681273c071959541b7a4acd
luna-spr <luna@spreatics.com>
2023-12-20 13:58:07 +0900

    add react styles code

---
+++ 31-styles/src/App.js
@@ -0,0 +1,24 @@
+import CssModuleComponents from './CssModuleComponents';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* App.css, index.css 파일 작성 */}
+      <h1>React Styling</h1>
+
+      <h2>CSS Module</h2>
+      <CssModuleComponents />
+
+      <h2>SASS</h2>
+      <SassComponent />
+
+      <h2>styled-components</h2>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
